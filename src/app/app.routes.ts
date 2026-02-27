7d767004b8568c90dd1e710585a86a5f0df13db9
adem ziri <ziriadem33@gmail.com>
2026-02-27 14:16:59 +0100

    adding dashboard components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { LoginComponent } from './login/login.component';
 import { ApplicationComponent } from './application/application.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: HeroComponent },
   { path: 'login', component: LoginComponent },
   { path: 'application', component: ApplicationComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-constructor(private router: Router) {}
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+
+  constructor(private router: Router) { }
+
+  onLogin() {
+    if (!this.email || !this.password) {
+      this.errorMessage = 'Please fill in all fields.';
+      return;
+    }
+
+    if (!this.email.includes('@')) {
+      this.errorMessage = 'Please enter a valid email address.';
+      return;
+    }
+
+    
+    if (this.email === 'admin@edusmart.com' && this.password === 'admin123') {
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.errorMessage = 'Invalid email or password.';
+    }
+  }
 
   goBack() {
     this.router.navigate(['/']);
